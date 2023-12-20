7cc0e7d149c60c6f3ac83aca4c1bb0da797d153d
wasan <wasanawwad2020@gmail.com>
2023-12-20 19:43:21 +0200

    Next components (Image)

---
+++ app/component/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+// must start from root
+function Header() {
+  return (
+    <div>
+      
+        <image src='/1.png' width={200} height={300}/>
+    </div>
+  )
+}
+
+export default Header
